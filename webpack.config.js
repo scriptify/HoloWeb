71f6def5c9bba3a99d607e9c3d518bbcd7c75a69
scriptify <maximilian.torggler@tuta.io>
2016-11-08 15:59:09 +0100

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+var path = require('path'),
+webpack = require('webpack'), // Da bundling modules!
+NpmInstallPlugin = require('npm-install-webpack-plugin'), // Install client dependencies automatically!
+merge = require('webpack-merge'), // Merge together configurations!
+cssnext = require('postcss-cssnext');
+
+const PATHS = {
+  js: path.join(__dirname, './src/'),
+  build: path.join(__dirname, './build/')
+};
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const COMMON_CONFIGURATION = {
+  entry: {
+    app: PATHS.js
+  },
+  resolve: {
+    extensions: ['', '.js'], // Resolve these extensions
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+    library: 'HoloWeb'
+  },
+  module: {
+    loaders: [
+      {
+         test: /\.js$/,
+         loaders: ['babel?cacheDirectory'],
+         include: PATHS.js
+       },
+       {
+          test: /\.(jpe?g|png|gif|svg|mp3|wav)$/i,
+          loaders: [
+              'file?hash=sha512&digest=hex&name=[hash].[ext]'
+          ],
+          include: PATHS.js
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css', 'postcss'],
+          include: PATHS.app
+        }
+    ]
+  },
+  postcss: () => {
+    return [ cssnext ];
+  }
+};
+
+switch(TARGET) {
+  // Which procedure was started?
+  default:
+  case 'start:dev': {
+    module.exports = merge(COMMON_CONFIGURATION, {
+      devServer: {
+        contentBase: PATHS.build,
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+        stats: 'errors-only'
+      },
+      plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new NpmInstallPlugin({
+          save: true
+        })
+      ],
+      devtool: 'eval-source-map'
+    });
+  }
+  break;
+  case 'start:prod': {
+    module.exports = merge(COMMON_CONFIGURATION, {
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+          }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          compress: { warnings: false }
+        }),
+        new webpack.optimize.DedupePlugin()
+      ]
+    });
+  }
+}
